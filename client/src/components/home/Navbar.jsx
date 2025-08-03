f33bd0588937d7f75c043c37ac60e889977bbd92
MehediAhsan <mdmehedicse01@gmail.com>
2025-08-03 22:59:48 +0600

    inplemented clerk

--- client/src/components/home/Navbar.jsx
+++ client/src/components/home/Navbar.jsx
@@ -1,18 +1,24 @@
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const { user } = useUser();
+    const { openSignIn } = useClerk();
+
     return (
         <nav className="flex items-center justify-between py-2 px-4">
             <Link to='/' className="flex items-center gap-2">
                 <img src="/icon.png" alt="logo" className="w-8" />
                 <span className="text-xl font-semibold text-indigo-600">Craftwise.ai</span>
             </Link>
+            {
+                user ? <UserButton /> : <button onClick={openSignIn} className="bg-indigo-600 hover:bg-indigo-700 text-white text-sm px-5 py-2 rounded-full flex items-center space-x-1 transition">
+                    <span>get started</span>
+                    <ArrowRight className="w-4 h-4" />
+                </button>
+            }
 
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white text-sm px-5 py-2 rounded-full flex items-center space-x-1 transition">
-                <span>get started</span>
-                <ArrowRight className="w-4 h-4" />
-            </button>
         </nav>
     );
 };
