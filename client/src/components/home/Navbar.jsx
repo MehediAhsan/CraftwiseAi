cb879ff0eebe7c2f7f7cddcc398cbedb6727776a
MehediAhsan <mdmehedicse01@gmail.com>
2025-08-04 11:31:28 +0600

    added hero section

--- client/src/components/home/Navbar.jsx
+++ client/src/components/home/Navbar.jsx
@@ -6,14 +6,16 @@ const Navbar = () => {
     const { user } = useUser();
     const { openSignIn } = useClerk();
 
+    console.log(user);
+
     return (
-        <nav className="flex items-center justify-between py-2 px-4">
+        <nav className="flex items-center justify-between pt-5">
             <Link to='/' className="flex items-center gap-2">
                 <img src="/icon.png" alt="logo" className="w-8" />
-                <span className="text-xl font-semibold text-indigo-600">Craftwise.ai</span>
+                <span className="text-xl font-bold text-indigo-600 tracking-wider">CraftwiseAi</span>
             </Link>
             {
-                user ? <UserButton /> : <button onClick={openSignIn} className="bg-indigo-600 hover:bg-indigo-700 text-white text-sm px-5 py-2 rounded-full flex items-center space-x-1 transition">
+                user ? <UserButton /> : <button onClick={openSignIn} className="bg-indigo-600 hover:bg-indigo-700 text-white text-sm px-5 py-2 rounded-full flex items-center space-x-1 transition cursor-pointer">
                     <span>get started</span>
                     <ArrowRight className="w-4 h-4" />
                 </button>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/home/Navbar';
+import HeroSection from '../components/home/HeroSection';
 
 const Home = () => {
     return (
-        <div className='bg-gradient-to-r from-blue-100 via-pink-100 to-purple-100'>
+        <div className='bg-gradient-to-r from-blue-100 via-pink-100 to-purple-100 px-10 space-y-20'>
             <Navbar />
+            <HeroSection />
         </div>
     );
 };
