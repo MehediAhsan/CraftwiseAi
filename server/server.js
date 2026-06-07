230306f8f302d75e6dae0e9c5fdc85142682f45e
MehediAhsan <mdmehedicse01@gmail.com>
2026-06-07 11:47:20 +0600

    db create

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { clerkMiddleware, requireAuth } from '@clerk/express';
 
 dotenv.config();
 
@@ -10,10 +11,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(requireAuth());
 
 // Start the server
 app.listen(port, () => {
